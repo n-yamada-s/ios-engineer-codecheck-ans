0c784c2c8a1ed8df7f25acffdb06a0501799462b
n-yamada-s <natsu_83_angel@yahoo.co.jp>
2021-03-05 13:43:48 +0900

    SwiftLintのwarning対応 refs #2

--- iOSEngineerCodeCheck/ViewController2.swift
+++ iOSEngineerCodeCheck/ViewController2.swift
@@ -9,43 +9,38 @@
 import UIKit
 
 class ViewController2: UIViewController {
-    
+
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var languageLabel: UILabel!
-    
     @IBOutlet weak var starsLabel: UILabel!
     @IBOutlet weak var wathcersLabel: UILabel!
     @IBOutlet weak var forksLabel: UILabel!
     @IBOutlet weak var issuesLabel: UILabel!
-    
+
     var vc1: ViewController!
-        
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let repo = vc1.repo[vc1.idx]
-        
+
         languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
         starsLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
         wathcersLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
         forksLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
         issuesLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
-        
     }
-    
-    func getImage(){
-        
+
+    func getImage() {
         let repo = vc1.repo[vc1.idx]
-        
+
         titleLabel.text = repo["full_name"] as? String
-        
+
         if let owner = repo["owner"] as? [String: Any] {
             if let imgURL = owner["avatar_url"] as? String {
-                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
+                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, _, _) in
                     let img = UIImage(data: data!)!
                     DispatchQueue.main.async {
                         self.imageView.image = img
@@ -53,7 +48,6 @@ class ViewController2: UIViewController {
                 }.resume()
             }
         }
-        
     }
-    
+
 }
